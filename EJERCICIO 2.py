be29c4f5434488fbd1511e1c20316a6d2941582f
krc28 <restreka28@gmail.com>
2021-01-15 20:24:49 -0500

    listo

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -1,3 +1,19 @@
 """
 Diseñe un algoritmo que determine si ún número es o no es, par positivo.
 """
+"""
+INICIO
+var numero
+mostrar "escriba numero"
+leer numero
+if (numero % 2 == 0)
+    print ("es par")
+else 
+    print ("es impar")
+"""
+numero = input ("ingresa el numero:")
+dato = int(numero) % 2
+if (dato == 0):
+    print ("el numero es par")
+else:
+    print ("el numero es impar")
