82a2d50771d8b7fefa62b798574de2359a5dc734
krc28 <restreka28@gmail.com>
2021-01-15 21:17:11 -0500

    en camino

--- EJERCICIO 1.py
+++ EJERCICIO 1.py
@@ -4,13 +4,6 @@ Diseñe un algoritmo que determine cuanto debe pagar un cliente por el estaciona
 conociendo el tiempo de estacionamiento en horas y minutos.
 """
 """
-se que vale 1500 la hora o la fraccion, entonces tiene que subir el precio por cada hora y cada fracción
-pero no se como poner lo de que suba en cada fracción osea algo como que si los minutos son 15,30 y 45 aumente
-en 1 la cantidad osea sume una vez mas los 1500. lo otro es que en pseudocodigo se pone es lea los datos y no como preguntarle
-a la persona y luego ahi si que lea esos datos me enredo en cuando saber si debe ser cadena de texto o
-una variable que lea y asi pueda arrojar los resultados
-"""
-"""
 INICIO
  hora = numero de horas + 1 hora adicional dado que haya fracciones en el tiempo por los minutos
  minutos = numero de minutos creo
@@ -22,10 +15,12 @@ INICIO
   
  elif (minutos < "0"): no se si el cero si van en comillas o necesita ser leido ese dato de la variable establecida
     print (hora * costo)
- FIN IF
+FIN IF
 FIN   
 """
 horas = input("ingresa las horas de estacionamiento")
 minutos = input("ingresa los minutos de estacionamiento")
 costo = 1500
 tiempo = costo * horas
+if (minutos == "0"):
+   print (int(tiempo))
