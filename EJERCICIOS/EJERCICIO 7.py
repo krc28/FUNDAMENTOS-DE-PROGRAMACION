a2383c6a3fb09a089e03f70d89deb83120bb5bd7
krc28 <restreka28@gmail.com>
2021-02-13 15:07:34 -0500

    en proceso

--- EJERCICIOS/EJERCICIO 7.py
+++ EJERCICIOS/EJERCICIO 7.py
@@ -3,28 +3,30 @@ El promedio de prácticas de un curso se calcula en base a cuatro prácticas cal
 de las cuales se elimina la nota menor y se promedian las tres notas más altas.
 Diseñe un algoritmo que determine la nota eliminada y el promedio de prácticas de un estudiante.
  """
-nota_uno = str (input ("ingrese la primera nota de práctica: "))
-nota_dos = str (input ("ingrese la segunda nota de práctica: "))
-nota_tres = str (input ("ingrese la tercera nota de práctica: "))
-nota_cuatro = str (input ("ingrese la cuarta nota de práctica: "))
-primera_nota = str ((nota_dos >= nota_uno) and (nota_tres >= nota_uno) and (nota_cuatro >= nota_uno))
-segunda_nota = str ((nota_uno >= nota_dos) and (nota_tres >= nota_dos) and (nota_cuatro >= nota_dos))
-tercera_nota = str ((nota_uno >= nota_tres) and (nota_dos >= nota_tres) and (nota_cuatro >= nota_tres))
-cuarta_nota = str ((nota_uno >= nota_cuatro) and (nota_dos >= nota_cuatro) and (nota_tres >= nota_cuatro))
+nota1 = str (input ("ingrese la primera nota de práctica: "))
+nota2 = str (input ("ingrese la segunda nota de práctica: "))
+nota3 = str (input ("ingrese la tercera nota de práctica: "))
+nota4 = str (input ("ingrese la cuarta nota de práctica: "))
+primera_nota = (int (nota2 >= nota1) and (nota3 >= nota1) and (nota4 >= nota1))
+segunda_nota = (int (nota1 >= nota2) and (nota3 >= nota2) and (nota4 >= nota2))
+tercera_nota = (int (nota1 >= nota3) and (nota2 >= nota3) and (nota4 >= nota3))
+cuarta_nota = (int (nota1 >= nota4) and (nota2 >= nota4) and (nota3 >= nota4))
+promedio_notas = (nota1 + nota2 + nota3 + nota4)
 if (primera_nota):
-    nota_eliminada = primera_nota
-    promedio_notas = int ((nota_uno + nota_dos + nota_tres + nota_cuatro - nota_eliminada)) / 3
-    print ("la primera nota es la eliminada equivalente a: " + nota_uno)
+    nota_eliminada = nota1
+    promedio = (int (promedio_notas) - int (nota_eliminada)) / 3
+    print ("la primera nota es la eliminada equivalente a: " + nota1)
 elif (segunda_nota):
-    nota_eliminada = segunda_nota
-    promedio_notas = int ((nota_uno + nota_dos + nota_tres + nota_cuatro - nota_eliminada)) / 3
-    print ("la segunda nota es la eliminada equivalente a: " + nota_dos)
+    nota_eliminada = nota2
+    promedio = (int (promedio_notas) - int (nota_eliminada)) / 3
+    print ("la segunda nota es la eliminada equivalente a: " + nota2)
 elif (tercera_nota):
-    nota_eliminada = tercera_nota
-    promedio_notas = int ((nota_uno + nota_dos + nota_tres + nota_cuatro - nota_eliminada)) / 3
+    nota_eliminada = nota3
+    promedio = (int (promedio_notas) - int (nota_eliminada)) / 3
+    print ("la tercera nota es la eliminada equivalente a: " + nota3)
 else:
     (cuarta_nota)
-    nota_eliminada = cuarta_nota
-    promedio_notas = int ((nota_uno + nota_dos + nota_tres + nota_cuatro - nota_eliminada)) / 3
-    print ("la cuarta nota es la eliminada equivalente a: " + nota_cuatro)
-print ("el promedio de prácticas del estudiante es: " + promedio_notas)
+    nota_eliminada = nota4
+    promedio = (int (promedio_notas) - int (nota_eliminada)) / 3
+    print ("la cuarta nota es la eliminada equivalente a: " + nota4)
+print ("el promedio de prácticas del estudiante es: " + str (promedio))
