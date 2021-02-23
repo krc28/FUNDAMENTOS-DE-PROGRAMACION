c146c136481af4b1230a91a9a7805d566f58b628
krc28 <restreka28@gmail.com>
2021-02-23 16:07:24 -0500

    en proceso

--- EJERCICIOS/EJERCICIO 9.py
+++ EJERCICIOS/EJERCICIO 9.py
@@ -2,3 +2,7 @@
 Diseñe un algoritmo que lea un número entero de 3 cifras, y forme el mayor número posible con las cifras del número ingresado. 
 El número formado debe tener el mismo signo que el número ingresado. 
 """
+numero = int (input ("ingrese el número: "))
+centena = (numero / 100)
+unidad = (numero /10)
+decena =
