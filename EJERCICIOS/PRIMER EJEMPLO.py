dcb93bd4bb94a35c3bf1c1056028e5ec8aac2458
krc28 <restreka28@gmail.com>
2021-06-07 20:09:54 -0500

    listo

--- EJERCICIOS/PRIMER EJEMPLO.py
+++ EJERCICIOS/PRIMER EJEMPLO.py
@@ -1,4 +1,4 @@
-print ("hola soy pochinki")
+"""print ("hola soy pochinki")
 culo = "grande"
 tetas = 2
 print (culo + str(tetas))
@@ -8,4 +8,18 @@ if (estatura >= 180):
 elif (estatura == 155):
     print ("eres pochinki")
 else:
-    print ("eres enano")
+    print ("eres enano")"""
+V = [8, 3, 1, 6, 2, 7, 4, 9, 5]
+for i in range(1, 5):
+    a = V[i]
+    V[i] = V[8-i+1]
+    V[8-i+1] = a
+for i in range(0, 8):
+    print(V[i], end="", sep=",")
+
+ 
+
+
+
+
+
