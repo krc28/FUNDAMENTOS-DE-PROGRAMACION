99bc7f0eb76ae97e2ad38e9ebacedfb3a7f790f3
krc28 <restreka28@gmail.com>
2021-06-09 20:01:19 -0500

    listo

--- EJERCICIOS/EJERCICIO 1.py
+++ EJERCICIOS/EJERCICIO 1.py
@@ -18,11 +18,32 @@ INICIO
 FIN IF
 FIN   
 """
-horas = int(input("ingresa las horas de estacionamiento "))
+"""horas = int(input("ingresa las horas de estacionamiento "))
 minutos = int(input("ingresa los minutos de estacionamiento "))
 costo = 1500
 tiempo = costo * horas
 if (minutos == 0):
    print (tiempo )
 elif (minutos >= 1):
-   print (tiempo + costo )
+   print (tiempo + costo )"""
+
+"""import random
+def __init__(self, m, n):
+   self.m = m 
+   self.n = n 
+   m = 6
+   n = 8
+   self.mat = [0] * (m + 1)
+   for i in range(m + 1):
+      a = [0] * (n + 1)
+      self.mat.append(a)     
+def construyeMatriz(self, r = 100):
+   for i in range(1, self.m + 1):
+      for j in range(1, self.n +1):
+         self.mat[i][j] = random.randint(1, r)
+def imprimeMatrizPorFilas(self, mensaje="Matriz sin nombre "):
+   print("\n", mensaje)
+   for i in range(1, self.m + 1):
+      for j in range(1, self.n + 1):
+         print(self.mat[i][j], "\t", end="")"""
+         
