080bcfaad83507123f1f57fe65d3a633273d890b
krc28 <restreka28@gmail.com>
2021-06-24 16:20:22 -0500

    progreso

---
+++ EJERCICIOS/TKINTER.py
@@ -0,0 +1,22 @@
+from tkinter import * 
+main_window = Tk()
+main_window.title ("Pochinki")
+main_frame = Frame (main_window, 
+                    bg = "#F0F8FF", 
+                    height = 500 , 
+                    width = 500 , 
+                    padx = 50 , 
+                    pady = 50, 
+                    cursor = "arrow")
+main_frame.pack()
+title_label = Label(main_frame, 
+                    text = "Hola soy Pochinki Pochinokona",
+                    font= ("Times New Roman", 18),
+                    fg = "#FF1493",
+                    justify= CENTER)
+title_label.pack()
+imagen = PhotoImage(file = "pochinki.png")
+img_label = Label(main_frame,
+                  image = imagen)
+img_label.pack()
+main_window.mainloop()
