81b9a1e42f472b9692def4bf048895560b52796d
krc28 <restreka28@gmail.com>
2021-06-24 17:25:28 -0500

    en proceso

--- EJERCICIOS/TKINTER.py
+++ EJERCICIOS/TKINTER.py
@@ -2,7 +2,7 @@ from tkinter import *
 main_window = Tk()
 main_window.title ("Pochinki")
 main_frame = Frame (main_window, 
-                    bg = "#F0F8FF", 
+                    bg = "#FF1493", 
                     height = 500 , 
                     width = 500 , 
                     padx = 50 , 
@@ -15,8 +15,12 @@ title_label = Label(main_frame,
                     fg = "#FF1493",
                     justify= CENTER)
 title_label.pack()
-imagen = PhotoImage(file = "pochinki.png")
+imagen = PhotoImage(file = "EJERCICIOS\pochinki.png")
 img_label = Label(main_frame, 
-                  image = imagen,)
+                  image = imagen,
+                  bg = "#FF1493")              
 img_label.pack()
+boton = Button (main_frame,
+                text = "Salúdame")
+boton.pack()
 main_window.mainloop()
