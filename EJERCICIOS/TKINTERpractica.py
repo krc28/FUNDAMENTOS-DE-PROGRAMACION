3550b4ef3271fe338b27036b10121e0ba41db7a4
krc28 <restreka28@gmail.com>
2021-06-25 12:59:49 -0500

    listo

--- EJERCICIOS/TKINTERpractica.py
+++ EJERCICIOS/TKINTERpractica.py
@@ -1,6 +1,6 @@
 from tkinter import * 
 main_window = Tk()
-main_window.title ("Pochinki")
+main_window.title ("Chiss")
 main_frame = Frame (main_window, 
                     bg = "#FF1493", 
                     height = 500 , 
@@ -10,17 +10,28 @@ main_frame = Frame (main_window,
                     cursor = "arrow")
 main_frame.pack()
 title_label = Label(main_frame, 
-                    text = "Hola soy Pochinki Pochinokona",
+                    text = "Hola soy el animalito",
                     font= ("Times New Roman", 18),
                     fg = "#FF1493",
                     justify= CENTER)
 title_label.pack()
-imagen = PhotoImage(file = "EJERCICIOS\pochinki.png")
+imagen = PhotoImage(file = "EJERCICIOS\chis.png")
 img_label = Label(main_frame, 
                   image = imagen,
-                  bg = "#FF1493")              
+                  bg = "#FF1253")              
 img_label.pack()
+def accion_button():
+    global boton_text
+    if boton_text == "Saluda a chis aquí":
+        boton_text = "Hola chis"
+    else:
+        boton_text = "Saluda a chis aquí"
+    boton.config(text = boton_text)
+boton_text = "Saluda a chis aquí"
 boton = Button (main_frame,
-                text = "Salúdame")
+                text = "Saluda a chis aquí",
+                bg = "#FF5522",
+                fg = "#FF0652",
+                command= accion_button)
 boton.pack()
 main_window.mainloop()
